9b253030a588e0a3402898a87ba2c00fea2d383b
Kim Faughnan <faugh22k@mtholyoke.edu>
2014-04-30 23:31:27 -0400

    More debugging due date sorting
    Just more print statements

--- src/TaskOrderStrictDueDate.java
+++ src/TaskOrderStrictDueDate.java
@@ -11,12 +11,16 @@ public class TaskOrderStrictDueDate implements TaskOrderer{
     	
     	System.out.println("one.priority = " + one.toString());
     	System.out.println("two.priority = " + two.toString());
+    	
+    	System.out.println("one.text = " + one.getText());
+    	System.out.println("two.text = " + two.getText());
+    	
         // the task with earlier due date comes first
         if(one.getDue().before(two.getDue())){
-        	System.out.println("one is due before two");
+        	System.out.println("one is due before two\n");
             return -1;
         } else if (one.getDue().after(two.getDue())){
-        	System.out.println("one is due after two");
+        	System.out.println("one is due after two\n");
             return 1;
         }
 
@@ -24,14 +28,14 @@ public class TaskOrderStrictDueDate implements TaskOrderer{
         
         // if the dates are the same, ordered by priority
         if(one.getImportance().ordinal() > two.getImportance().ordinal()){
-        	System.out.println("one has a higher priority than two");
+        	System.out.println("one has a higher priority than two\n");
             return -1;
         } else if (one.getImportance().ordinal() < two.getImportance().ordinal()){
-        	System.out.println("one has a lower priority than two");
+        	System.out.println("one has a lower priority than two\n");
             return 1;
         }
 
-        System.out.println("one and two due at same time, have same importance");
+        System.out.println("one and two due at same time, have same importance\n");
         // if dates and priority level are the same, they are equal
         return 0;
     }
